be1e2cbd61c3467567909ace157c869bf89d830c
Alakhshrivastava <shrivastavaalakh26@gmail.com>
2025-05-10 02:37:04 +0530

    ChapterCard & ConformingChapter Error Resolved and Youtube API Integrated to return Video and Transcript

--- app/api/chapter/getInfo/route.ts
+++ app/api/chapter/getInfo/route.ts
@@ -1,19 +1,87 @@
 // /api/chapter/getInfo
 
 import { NextResponse } from "next/server";
+import { z } from "zod";
+import { prisma } from "@/lib/db";
+import { getQuestionsFromTranscript, getTranscript, searchYouTube } from "@/lib/youtube";
+import { strict_output } from "@/lib/gpt";
 
-const sleep = async () => 
-    new Promise((resolve) => {
-        setTimeout(resolve, Math.random() * 4000 );
-    });
+
+const bodyParser = z.object({
+  chapterId: z.string(),
+})
 
 
 export async function POST(req: Request, res: Response) {
   try {
-    await sleep();
-    return NextResponse.json({ message :"hello" });
+    const body =await req.json();
+    const { chapterId } = bodyParser.parse(body);
+
+    const chapter =await prisma.chapter.findUnique({
+      where:{
+        id:chapterId,
+      },
+    });
+    if (!chapter) {
+      return NextResponse.json({
+        Success: false,
+        error: "Chapter not found",
+      },{ status: 404 });
+    }
+
+    const videoId= await searchYouTube(chapter.youtubeSearchQuery);
+    let transcript = await getTranscript(videoId);
+    let maxLength = 500;
+    transcript = transcript.split(' ').slice(0, maxLength).join(' ');
+
+    const {summary}:{summary:string} = await strict_output(
+      'you are an AI capable of summarising a youtube video transcript.',
+      'summarise the transcript in 250 words or less and do not talk of the sponsors or anything unrelated to the main topic of the video, also do not introduce what the summary is about.\n'+ transcript,
+      {
+        summary: "summary of the transcript"
+      }
+    );
+
+    const questions = await getQuestionsFromTranscript(transcript, chapter.name)
+
+    await prisma.question.createMany({
+      data: questions.map(question => {
+        let options = [question.answer, question.option1, question.option2, question.option3];
+        options = options.sort(() => Math.random() - 0.5);
+        return{
+          question:question.question,
+          answer:question.answer,
+          options:JSON.stringify(options),
+          chapterId:chapterId,
+        }
+      }),
+    });
+
+    await prisma.chapter.update({
+      where: {
+        id: chapterId,
+      },
+      data:{
+        videoId: videoId,
+        summary: summary,
+      },
+    });
+
+    return NextResponse.json({success:true});
+
   } catch (error) {
-    
+    if(error instanceof z.ZodError) {
+      return NextResponse.json({
+        Success: false,
+        error: "Invalid Body",
+      },{ status: 400 });
+    }
+    else{
+      return NextResponse.json({
+        Success: false,
+        error: "Unknown",
+      },{ status: 500 });
+    }
   }
-  
+    
 }
--- components/ChapterCard.tsx
+++ components/ChapterCard.tsx
@@ -4,32 +4,72 @@ import React from 'react'
 import { cn } from '@/lib/utils'
 import { useMutation } from '@tanstack/react-query'
 import axios from 'axios'
-
+import { toast } from 'sonner'
+import { set } from 'zod'
+import { Loader2 } from 'lucide-react'
 
 
 type Props = {
     chapter: Chapter
     chapterIndex: number
+    completedChapters: Set<String>
+    setCompletedChapters: React.Dispatch<React.SetStateAction<Set<String>>>
 };
 
 export type ChapterCardHandler = {
     triggerLoad: () => void;
 }
 
-const ChapterCard = React.forwardRef<ChapterCardHandler, Props>(({chapter, chapterIndex}, ref) => {
-    React.useImperativeHandle(ref, () => ({
-        async triggerLoad(){
-            console.log("Hello")
-        }
-    }))
+const ChapterCard = React.forwardRef<ChapterCardHandler, Props>(({chapter, chapterIndex, setCompletedChapters, completedChapters}, ref) => {
 
     const[Success, setSuccess] = React.useState <boolean | null> (null);
     const {mutate: getChapterInfo, isPending}= useMutation({
         mutationFn: async ()=>{
-            const response =await axios.post('/api/chapter/getInfo')
+            const response =await axios.post('/api/chapter/getInfo',{chapterId: chapter.id});
             return response.data;
+        },
+    });
+
+    const addChapterIdToSet = React.useCallback(() => {
+        setCompletedChapters((prev) => {
+            const newSet = new Set(prev);
+            newSet.add(chapter.id);
+            return newSet;
+        });
+    }, [chapter.id, setCompletedChapters]);
+
+    React.useEffect(() => {
+        if(chapter.videoId){
+            setSuccess(true);
+            addChapterIdToSet();
         }
-    })
+    }, [chapter, addChapterIdToSet]);
+
+    React.useImperativeHandle(ref, () => ({
+        async triggerLoad(){
+            if(chapter.videoId){
+                addChapterIdToSet();
+                return;
+            }
+            getChapterInfo(undefined, {
+                onSuccess: () => {
+                    setSuccess(true);
+                    addChapterIdToSet();
+                },
+                onError:(error) => {
+                    console.error(error);
+                    setSuccess(false);  
+                    toast("Error",{
+                        description: "There was an error loading your chapter",
+                        action: "destructive",
+                    });
+                    addChapterIdToSet();
+                },
+                
+            });
+        },
+    }));
+
   return (
     <div key={chapter.id} className={cn('px-4 py-2 mt-2 rounded flex justify-between', {
         "bg-secondary": Success === null, 
@@ -40,6 +80,7 @@ const ChapterCard = React.forwardRef<ChapterCardHandler, Props>(({chapter, chapt
         <h5>
             {chapter.name}
         </h5>
+        {isPending && <Loader2 className='animate-spin'/>}
     </div>
   )
 });
@@ -47,3 +88,95 @@ const ChapterCard = React.forwardRef<ChapterCardHandler, Props>(({chapter, chapt
 ChapterCard.displayName = 'ChapterCard';
 
 export default ChapterCard
+
+// "use client";
+// import { cn } from "@/lib/utils";
+// import { Chapter } from "@prisma/client";
+// import { useMutation } from "@tanstack/react-query";
+// import axios from "axios";
+// import React from "react";
+// import {toast} from "sonner"
+// import { Loader2 } from "lucide-react";
+
+// type Props = {
+//   chapter: Chapter;
+//   chapterIndex: number;
+//   completedChapters: Set<String>;
+//   setCompletedChapters: React.Dispatch<React.SetStateAction<Set<String>>>;
+// };
+
+// export type ChapterCardHandler = {
+//   triggerLoad: () => any;
+// };
+
+// const ChapterCard = React.forwardRef<ChapterCardHandler, Props>(
+//   ({ chapter, chapterIndex, setCompletedChapters, completedChapters }, ref) => {
+//     const { toast } = useToast();
+//     const [success, setSuccess] = React.useState<boolean | null>(null);
+//     const { mutate: getChapterInfo, isPending } = useMutation({
+//       mutationFn: async () => {
+//         const response = await axios.post("/api/chapter/getInfo", {
+//           chapterId: chapter.id,
+//         });
+//         return response.data;
+//       },
+//     });
+
+//     const addChapterIdToSet = React.useCallback(() => {
+//       setCompletedChapters((prev) => {
+//         const newSet = new Set(prev);
+//         newSet.add(chapter.id);
+//         return newSet;
+//       });
+//     }, [chapter.id, setCompletedChapters]);
+
+//     React.useEffect(() => {
+//       if (chapter.videoId) {
+//         setSuccess(true);
+//         addChapterIdToSet;
+//       }
+//     }, [chapter, addChapterIdToSet]);
+
+//     React.useImperativeHandle(ref, () => ({
+//       async triggerLoad() {
+//         if (chapter.videoId) {
+//           addChapterIdToSet();
+//           return;
+//         }
+//         getChapterInfo(undefined, {
+//           onSuccess: () => {
+//             setSuccess(true);
+//             addChapterIdToSet();
+//           },
+//           onError: (error) => {
+//             console.error(error);
+//             setSuccess(false);
+//             toast({
+//               title: "Error",
+//               description: "There was an error loading your chapter",
+//               variant: "destructive",
+//             });
+//             addChapterIdToSet();
+//           },
+//         });
+//       },
+//     }));
+//     return (
+//       <div
+//         key={chapter.id}
+//         className={cn("px-4 py-2 mt-2 rounded flex justify-between", {
+//           "bg-secondary": success === null,
+//           "bg-red-500": success === false,
+//           "bg-green-500": success === true,
+//         })}
+//       >
+//         <h5>{chapter.name}</h5>
+//         {isLoading && <Loader2 className="animate-spin" />}
+//       </div>
+//     );
+//   }
+// );
+
+// ChapterCard.displayName = "ChapterCard";
+
+// export default ChapterCard;
